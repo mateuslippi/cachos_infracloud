e2d6f855824f91b0a70b685007831d47bdcafd05
Mateus Lippi <mlippi@uolinc.com>
2023-09-21 17:18:31 -0300

    Ajustado função de dependencias

--- libs/functions_deps.sh
+++ libs/functions_deps.sh
@@ -1,8 +1,8 @@
-function buscar_distro () {
+function _buscar_distro () {
     grep ^ID= /etc/os-release | cut -d = -f 2
 }
-function install_sshpass () {
-    case "$(buscar_distro)" in
+function _install_sshpass () {
+    case "$(_buscar_distro)" in
         ubuntu) sudo apt-get install -y sshpass
         ;;
         debian) sudo apt-get install -y sshpass
