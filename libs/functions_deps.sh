319df38dd1044a3de4a467ce76121f9d9875b52e
Mateus Lippi <mlippi@uolinc.com>
2023-09-25 14:10:01 -0300

    Aprimorando a instalação automática das dependências.

--- libs/functions_deps.sh
+++ libs/functions_deps.sh
@@ -1,15 +1,40 @@
-function _buscar_distro () {
-    grep ^ID= /etc/os-release | cut -d = -f 2
+function _find_distribution () {
+    #Maior parte das distros.
+    if [ -f /etc/os-release ]; then
+        grep ^ID= /etc/os-release | cut -d = -f 2
+    else #RedHat...
+        if [ -f /etc/redhat-release ]; then
+            echo "rhel"
+        else
+            echo "other"
+        fi
+    fi
 }
+
+function _deps () {
+    echo -e "\nPrecisamos instalar as dependências: \n"
+}
+
+function _error_deps () {
+    echo -e "\nO programa não conseguiu instalar o sshpass, instale-o manualmente.\n" && exit 1
+}
+
+function _post_deps () {
+    echo -e "\nDependências instaladas, execute novamente o programa." && exit 0
+}
+
 function _install_sshpass () {
-    case "$(_buscar_distro)" in
-        ubuntu) sudo apt-get install -y sshpass
+    case "$(_find_distribution)" in
+        ubuntu) _deps && sudo apt-get install -y sshpass && _post_deps || _error_deps
         ;;
-        debian) sudo apt-get install -y sshpass
+        debian) _deps && sudo apt-get install -y sshpass && _post_deps || _error_deps
         ;;
-        fedora) yum install -y sshpass
+        fedora) _deps && sudo yum install -y sshpass && _post_deps || _error_deps
         ;;
-        ol) sudo yum install -y sshpass
+        ol) _deps && sudo yum install -y sshpass && _post_deps || _error_deps
         ;;
+        rhel) _deps && sudo yum install -y sshpass && _post_deps || _error_deps
+        ;;
+        other) _error_deps
     esac
 }
